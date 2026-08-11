ba9c42f55cda0d346130c43bd3a725d81b7f4436
abhadhami <vikramsinghdhami52@gmail.com>
2026-08-11 14:08:38 +0530

    Load premium section styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+  const stylesheet = document.createElement('link');
+  stylesheet.rel = 'stylesheet';
+  stylesheet.href = 'css/additions.css';
+  document.head.appendChild(stylesheet);
+});
