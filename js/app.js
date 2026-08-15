13a194bfd5a8cc9245982e4da618b5e474c1652d
abhadhami <vikramsinghdhami52@gmail.com>
2026-08-15 17:27:44 +0530

    Load smart assessment module

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
   stylesheet.rel = 'stylesheet';
   stylesheet.href = 'css/additions.css';
   document.head.appendChild(stylesheet);
+
+  const assessmentScript = document.createElement('script');
+  assessmentScript.src = 'js/assessment.js';
+  assessmentScript.defer = true;
+  document.body.appendChild(assessmentScript);
 });
