79567d675d84011b86911ee2060e6c4f48af39fe
abhadhami <vikramsinghdhami52@gmail.com>
2026-08-16 13:59:37 +0530

    Connect refined fitness experience assessment

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
   programScript.onload = function () {
     const assessmentScript = document.createElement('script');
     assessmentScript.src = 'js/assessment.js';
+    assessmentScript.onload = function () {
+      const experienceScript = document.createElement('script');
+      experienceScript.src = 'js/assessment-experience.js';
+      document.body.appendChild(experienceScript);
+    };
     assessmentScript.defer = true;
     document.body.appendChild(assessmentScript);
   };
